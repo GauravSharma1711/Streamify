5cefad4b6d091ac1ec9964295609ea7fa1abb594
Gaurav Sharma <gau1711sha@gmail.com>
2025-05-20 15:52:10 +0530

    project complete

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -106,7 +106,7 @@ export const login = async(req,res)=>{
         res.cookie("jwt",token,
             {
                 maxAge:7*24*60*60*1000,
-                httpOny:true,
+                httpOnly:true,
                 sameSite:"strict",
                 secure:process.env.NODE_ENV==="production",
             })
@@ -154,10 +154,16 @@ export const onboard = async(req,res)=>{
         }
 
 
-        const updatedUser = await User.findOneAndUpdate(userId,{
-         ...req.body,
-         isOnboarded:true
-        },{new:true})
+    
+const updatedUser = await User.findOneAndUpdate(
+  { _id: userId },
+  {
+    ...req.body,
+    isOnboarded: true
+  },
+  { new: true }
+);
+
 
 
         if(!updatedUser){
